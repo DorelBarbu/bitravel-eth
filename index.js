5e558c5eb693ab01e4d50dcd4df835d6174f4dd0
Dorel Barbu <dorelbarbu96@gmail.com>
2019-06-30 03:00:47 +0300

    Changed default port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./src/routes/router');
 const Logger = require('./logger');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3200;
 
 app.listen(PORT, () => {
   Logger.success(`Listening on port ${PORT}`);
